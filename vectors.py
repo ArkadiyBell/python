cdacbf7212b1a8e6b7448e47f61d06edf5edd81a
ArkadiyBell <bells.vtv@gmail.com>
2024-11-05 22:07:18 +0800

    Вектора

---
+++ vectors.py
@@ -0,0 +1,38 @@
+import math
+class Vector2d:
+    def __init__(self,x1,y1,x2,y2):
+        self.x1=x1
+        self.x2=x2
+        self.y1=y1
+        self.y2=y2
+
+        self.x=x2-x1
+        self.y=y2-y1
+    def length(self):
+        x=self.x
+        y=self.y
+        return math.sqrt((x)**2+(y)**2)
+    def angle(self):
+        x=self.x
+        y=self.y
+        an=math.degrees(math.atan(abs(x)/abs(y)))
+        return an if x>=0 else an+90
+    def summ(self, vec):
+        x=self.x
+        y=self.y
+        res=Vector2d(0,0 ,x+vec.x,y+vec.y)
+        return [res.x, res.y]
+    def diff(self, vec):
+        x=self.x
+        y=self.y
+        res=Vector2d(0,0 ,x-vec.x,y-vec.y)
+        return [res.x, res.y]
+    def multiply(self,vec):
+        x=self.x
+        y=self.y
+        return x*vec.x+y*vec.y
+
+
+vector1=Vector2d(*list(map(int, input("Введите через пробел координаты начала и конца первого вектора").split())))
+vector2=Vector2d(*list(map(int, input("Введите через пробел координаты начала и конца второго вектора").split())))
+print(vector1.length(),vector1.angle(), vector1.summ(vector2),vector1.diff(vector2), vector1.multiply(vector2))
