682e2e22b8f544c1ec523dbb9257d3ba50cc6d5f
ArkadiyBell <bells.vtv@gmail.com>
2024-12-18 15:46:53 +0800

    final

--- gens.py
+++ gens.py
@@ -11,7 +11,8 @@ def fibonachi_gen(n):
             yield b 
             b+=a
             c+=1
-            
+   
+print('8 первых 8 чисел Фибоначчи: ')
 print([_ for _ in fibonachi_gen(8)])
 
 ###############################################################################
@@ -33,6 +34,7 @@ def prime_mn_gen(n):
             yield i
     
 
+print('Простые делители числа 2*3*5*19*101:')
 print([_ for _ in prime_mn_gen(2*3*5*19*101)])
 
 ###############################################################################
@@ -42,6 +44,7 @@ def degree_gen(n, degree):
         yield n**i
 
 a = 3
+print('Возведение числа 3 в степени от 1 до 15')
 print([_ for _ in degree_gen(a, 15)])
 
 ###############################################################################
@@ -50,6 +53,7 @@ def primes_in_range_gen(start, stop):
     for i in range(start+1, stop):
         if is_prime(i):
             yield i
+print('Простые числа в диапазоне (-100, 900)')
 print([_ for _ in primes_in_range_gen(-100, 900)])
     
     
